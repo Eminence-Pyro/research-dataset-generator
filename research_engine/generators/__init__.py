2b835dc1204dbb8450d71d632e510b9a4d4805a4
Eminence-Pyro <divinennata@gmail.com>
2026-07-10 17:36:29 +0100

    structure: generators package init

---
+++ research_engine/generators/__init__.py
@@ -0,0 +1,7 @@
+"""
+research_engine/generators/
+Produces synthetic research data as domain model objects.
+
+Depends on: research_engine/models/
+Does NOT depend on: parsers, validators, exporters, analysis, reports
+"""
