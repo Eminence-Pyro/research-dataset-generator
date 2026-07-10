40b43cb478f655d274d2e57ef58439aff4ba376b
Eminence-Pyro <divinennata@gmail.com>
2026-07-10 17:36:43 +0100

    structure: validators package init

---
+++ research_engine/validators/__init__.py
@@ -0,0 +1,7 @@
+"""
+research_engine/validators/
+Checks Dataset and domain objects for statistical and logical consistency.
+
+Depends on: research_engine/models/
+Does NOT depend on: parsers, generators, exporters, analysis, reports
+"""
